2e8e8860bf1c8f498a46cf9be7d6d8e7dccf1b00
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudLocationfinderV1/CloudLocation.swift
+++ Sources/GoogleCloudLocationfinderV1/CloudLocation.swift
@@ -133,7 +133,7 @@ public struct CloudLocation: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:CloudProvider/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "CLOUD_PROVIDER_UNSPECIFIED": self = .unspecified
@@ -147,7 +147,7 @@ public struct CloudLocation: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:CloudProvider/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
@@ -253,7 +253,7 @@ public struct CloudLocation: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:CloudLocationType/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "CLOUD_LOCATION_TYPE_UNSPECIFIED": self = .unspecified
@@ -267,7 +267,7 @@ public struct CloudLocation: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:CloudLocationType/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
